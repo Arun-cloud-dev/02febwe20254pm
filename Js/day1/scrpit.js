e9e881df472a54ccb570bdc178265c23578b6af2
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-03-25 20:47:34 +0530

    qwr

---
+++ Js/day1/scrpit.js
@@ -0,0 +1,45 @@
+// // var a = 10;
+// // var b = 20;
+
+
+
+// // console.log(a +b);
+
+
+// let  a = 20;
+// let b = 30; 
+
+// console.log(a + b);
+
+
+// {
+// let a = 40;
+// }
+
+
+
+// console.log(a + b);
+
+
+
+
+
+
+
+// {
+//     let arun = "aruna";
+//     console.log(arun);
+// }
+
+// let a;
+// a = 10;
+
+// const a = 10; 
+
+
+// var ar = 40;
+
+
+
+
+// let ar = 50;
