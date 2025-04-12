0612bc84cc55270596a54b8b129162bb1cbbddeb
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-04-12 17:58:57 +0530

    js

---
+++ Js/day3/Arrays.js
@@ -0,0 +1,49 @@
+//arrays 
+
+
+
+// const fruits = ["apple", "banana", "cherry"];
+
+
+// console.log(fruits[0]); // Output: apple
+// console.log(fruits[2]);
+
+
+// fruits.push("Grapes")
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+// fruits.unshift("Mango");
+// console.log(fruits);
+
+
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const squaredNumbers = numbers.map((number) => number * number);
+
+// console.log(squaredNumbers);
+
+// const even = numbers.filter(num => num % 2 === 0) //what is the output of this line
+// console.log(even); // Output: [2, 4]
+
+
+// const sum = numbers.reduce((total, num) => total + num, 0);
+// console.log(sum); 
+
+// numbers.forEach(num => console.log(num * 2));
+// // Output: 2, 4, 6, 8, 10
+
+
+
+// const matrix = [
+//   [1, 2],
+//   [3, 4, [45,90,[35]]],
+//   [5, 6],
+// ];
+
+
+
+// console.log(matrix[1][2][2][0]); // Output: 5
---
+++ Js/day3/prototypes.js
@@ -0,0 +1,15 @@
+// function Person(name) {
+//     this.name = name
+// }
+
+
+// Person.prototype.greet = function () {
+//     console.log("Hello , I Am " + this.name);
+// }
+
+// const p1 = new Person("John");
+// const p2 = new Person("Jane");
+
+
+// p1.greet();
+// p2.greet();
